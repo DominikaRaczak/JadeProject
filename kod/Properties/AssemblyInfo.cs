f341dc98cd1686ec90291b26dc753c433012e262
dominikaraczak94 <dominika.raczak@gmail.com>
2018-05-06 13:14:51 +0200

    first commit

---
+++ kod/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+[assembly: AssemblyTitle("JadeProject")]
+[assembly: AssemblyDescription("Jade classes")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly:AssemblyVersion("16.0.02")]
+[assembly:AssemblyFileVersion("16.0.02")]
+[assembly:AssemblyCompany("Jade Software Corporation Limited")]
+[assembly:AssemblyProduct("JADE")]
+[assembly:AssemblyCopyright("Copyright (c) Jade Software Corporation Limited 2008")]
